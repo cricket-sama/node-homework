72229f77ae9cf54e493482d614c536fb17f6986e
cricket-sama <156969289+cricket-sama@users.noreply.github.com>
2026-04-06 17:24:09 -0400

    completed week 1 assignment

--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -8,11 +8,45 @@ if (!fs.existsSync(sampleFilesDir)) {
 }
 
 // OS module
-
+console.log('Platform: ', os.platform());
+console.log('CPU: ', os.cpus()[0].model);
+console.log('Total Memory: ', os.totalmem());
 
 // Path module
+console.log('Joined path: ', path.join(sampleFilesDir, 'sample.txt'));
 
 // fs.promises API
+const demoFilePath = path.join(sampleFilesDir, 'demo.txt');
 
+fs.promises.writeFile(demoFilePath, 'Hello from fs.promises!')
+  .then(() => fs.promises.readFile(demoFilePath, 'utf-8'))
+  .then(contents => console.log('fs.promises read: ', contents))
+  .catch(err => console.error(err.message));
 
 // Streams for large files- log first 40 chars of each chunk
+const largeFilePath = path.join(sampleFilesDir, 'largefile.txt');
+
+function largeFileText () {
+  let str = '';
+  for (let i = 0; i < 100; i++) {
+    str = str + 'This is a line in a large file...\n'}
+    return str;
+};
+
+fs.promises.writeFile(largeFilePath, largeFileText())
+  .then (() => {
+    const stream = fs.createReadStream(largeFilePath, { encoding: 'utf-8', highWaterMark: 1024 });
+
+    stream.on('data', (chunk) => {
+      console.log('Read chunk: ', chunk.slice(0, 40));
+    });
+
+    stream.on('end', () => {
+      console.log('Finished reading large file with streams.');
+    });
+
+    stream.on('error', (err) => {
+      console.error('Stream error: ', err.message);
+    });
+  })
+  .catch(err => console.log(err));
--- assignment1/node-fundamentals.md
+++ assignment1/node-fundamentals.md
@@ -1,25 +1,30 @@
 # Node.js Fundamentals
 
 ## What is Node.js?
-Answer here..
+Node.js is a runtime environment that executes code outside the browser
 
 ## How does Node.js differ from running JavaScript in the browser?
-Answer here..
+Node.js can access the file system, as well as open a web server socket. You can also execute code in the terminal
 
 ## What is the V8 engine, and how does Node use it?
-Answer here..
+The V8 engine executes code within Chrome. With Node, the V8 engine is used to execute JavaScript outside the browser
 
 ## What are some key use cases for Node.js?
-Answer here..
+- Streaming, because it has stream modules built in and data can be processed in chunks
+- Real-time applications because it can process I/O requests without blocking the event loop
 
 ## Explain the difference between CommonJS and ES Modules. Give a code example of each.
 
 **CommonJS (default in Node.js):**
 ```js
-// Answer here..
+// CJS is synchronous and uses require() and module exports
+const math = require('./math.js'); // importing a module
+module.exports = { add, multiply } // exporting functions
 ```
 
 **ES Modules (supported in modern Node.js):**
 ```js
-// Answer here..
+// ES Modules are the more modern standard, using import and export
+import { add, multiply } from './math.js'; //importing a module
+export { add, multiply } // exporting functions
 ``` 
