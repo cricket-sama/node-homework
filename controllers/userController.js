7e7f9c6cf7532ab0762058324acfb869477ea936
cricket-sama <156969289+cricket-sama@users.noreply.github.com>
2026-05-04 15:54:36 -0400

    assignment 5 completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const { userSchema } = require('../validation/userSchema');
 const crypto = require('crypto');
 const util = require('util');
 const scrypt = util.promisify(crypto.scrypt);
+const pool = require('../db/pg-pool');
 
 async function hashPassword(password) {
   const salt = crypto.randomBytes(16).toString('hex');
@@ -19,17 +20,23 @@ async function comparePassword(inputPassword, storedHash) {
 
 const logon = async (req, res) => {
     const { email, password } = req.body;
-    const existingUser = global.users.find((u) => {
-        return u.email === email;
-    });
+    const result = await pool.query(
+        'SELECT * FROM users WHERE email = $1', 
+        [email,]
+    );
 
-    if (!existingUser) {
+    if (result.rows.length === 0) {
         return res
           .status(StatusCodes.UNAUTHORIZED)
           .send({ message: 'Authentication Failed' });
     }
+
+    const user = result.rows[0];
     
-    const passwordResult = await comparePassword(password, existingUser.hashedPassword);
+    const passwordResult = await comparePassword(
+        password, 
+        user.hashed_password
+    );
 
     if (!passwordResult) {
         return res
@@ -37,33 +44,49 @@ const logon = async (req, res) => {
         .send({ message: 'Authentication Failed' });
     }
 
-    global.user_id = existingUser;
+    global.user_id = user.id;
     res.status(StatusCodes.OK).json({
-        name: existingUser.name,
-        email: existingUser.email
+        name: user.name,
+        email: user.email
     });
 };
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
     if (!req.body) req.body = {};
-    const {error, value} = userSchema.validate(req.body, { abortEarly: false });
+    const { error, value } = userSchema.validate(req.body, { abortEarly: false });
     if (error) {
-        return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ message: error.message })
+        return res.status(StatusCodes.BAD_REQUEST).json({ 
+            message: 'Validation failed',
+            details: error.details,
+        });
     }
-    const hashedPassword = await hashPassword(value.password);    
-    const { password, ...safeUser } = value;
-    const cleanedUser = {
-        ...safeUser,
-        hashedPassword
+    let user = null;
+    value.hashed_password = await hashPassword(value.password);
+    
+    try {
+        user = await pool.query(
+            `INSERT INTO users (email, name, hashed_password) 
+            VALUES ($1, $2, $3) RETURNING id, email, name`,
+            [value.email, value.name, value.hashed_password],
+        );
+    } catch (e) {
+    
+    if (e.code === '23505') {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: "Email already registered"
+        });
+    }
+    return next(e);
+    }
+    global.user_id = user.rows[0].id;
+    const cleanUser = {
+        name: user.rows[0]. name,
+        email: user.rows[0].email
     };
-    global.users.push(cleanedUser);
-    global.user_id = cleanedUser;
-    res.status(StatusCodes.CREATED).json(safeUser);
+    res.status(StatusCodes.CREATED).json(cleanUser);
 };
 
-const logoff = (req, res) => {
+const logoff = async (req, res) => {
     global.user_id = null;
     res.sendStatus(StatusCodes.OK);
 };
